4e513d7a84d0f5af0c1b936a6336262c68eefe94
bajian <313066164@qq.com>
2016-04-11 16:46:22 +0800

    Signed-off-by: bajian <313066164@qq.com>

---
+++ laravel.view.errors/404.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>23333</title>
+  <meta name="description" content="这是一个404页面">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<style type="text/css">
+  .text-center{text-align: center;}
+</style>
+</head>
+<body>
+
+<div class="text-center" style="margin-top: 100px">
+        <div style="vertical-align: center;"><img width="100%" style="max-width: 700px" src="http://static.hdslb.com/error/404.png" alt=""></div>
+</div>
+
+<div style="bottom: 10px;position: fixed;width: 90%;">
+  <div class="text-center">Copyright  2016 © 思路飞扬科技</div>
+</div>
+
+</body>
+</html>
---
+++ laravel.view.errors/500.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>访问出错</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<style type="text/css">
+*{margin: 0px}
+  .text-center{text-align: center;}
+}
+</style>
+</head>
+<body>
+
+<div class="text-center" style="margin-top: 100px">
+        <pre >
+          .----.
+       _.'__    `.
+   .--($)($$)---/#\
+ .' @          /###\
+ :         ,   #####
+  `-..__.-' _.-\###/
+        `;_:    `"'
+      .'"""""`.
+     /,  ya ,\\
+    //  500!  \\
+    `-._______.-'
+    ___`. | .'___
+   (______|______)
+        </pre></div>
+        <div class="text-center"><p>程序猿正在火速抢修中...</p>
+        <div>
+</div>
+
+<div style="bottom: 10px;position: absolute;width:100%;margin: 0 auto">
+  <div class="text-center">Copyright  2016 © 思路飞扬科技</div>
+</div>
+
+</body>
+</html>
